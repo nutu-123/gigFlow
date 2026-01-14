3fb815948edcd794edc37efb56d655ca1f9bed47
nutu-123 <phadtarenutan2004@gmail.com>
2026-01-14 20:31:11 +0530

    Switch to token-based authentication for production

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const AuthContext = createContext();
 
-const API_URL = import.meta.env.VITE_API_URL ;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
